a99d028ecf301d872eb34f84dc998cd44d98eef1
Morgan Van Valkenburgh <47067292+morgan-vanv@users.noreply.github.com>
2025-06-30 23:32:11 -0400

    MVP
    Gave plugin basic functionality, cleaned up, and outlined future todos and features

--- src/main/java/com/charliekicker/CharlieKickerPlugin.java
+++ src/main/java/com/charliekicker/CharlieKickerPlugin.java
@@ -1,15 +1,14 @@
 package com.charliekicker;
 
 import com.google.inject.Provides;
-import java.util.Arrays;
+
 import javax.inject.Provider;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.coords.LocalPoint;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.ClientTick;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.MenuEntryAdded;
 import net.runelite.api.events.MenuOptionClicked;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
@@ -21,9 +20,24 @@ import net.runelite.client.menus.MenuManager;
 @PluginDescriptor(
 	name = "CharlieKicker"
 )
+
+// getTarget = Charlie the Tramp
+// getType = 1003
+// getIdentifier = 26381
+
+// TODO:
+// 1. add a proper menu entry for ease of kicking
+// 2. say stuff randomly when doing so! "DIE DIE DIE" "I HATE U I HATE U I HATE U"
+// 3. make it so you can kick players? or all npcs?
+// 4. nail beast sounds every click
+// 5. allow usage of gloves/gauntlets to punch
+// 6. allow dynamic messages to be passed in
+// 7. use the config to allow enablement of punches, kicks, use on players, and objects like walls and chairs
+
+
+
 public class CharlieKickerPlugin extends Plugin
 {
-	private static final String KICK_CHARLIE = "KICK!";
 
 	@Inject
 	private Client client;
@@ -34,83 +48,59 @@ public class CharlieKickerPlugin extends Plugin
 	@Inject
 	private CharlieKickerConfig config;
 
-	private LocalPoint charlieLocation;
+	private boolean isKickQueued;
 
 	@Override
 	protected void startUp() throws Exception
 	{
 		log.info("CharlieKicker started!");
-		addMenuItem();
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		log.info("CharlieKicker stopped!");
-		removeMenuItem();
 	}
 
-	private synchronized void removeMenuItem() {
-		menuManager.get().removePlayerMenuItem(KICK_CHARLIE);
-	}
 
-	private synchronized void addMenuItem() {
-		if (client == null) return;
-		if (client.getNpcs().stream().noneMatch(npc -> npc.getName() != null && npc.getName().endsWith("Tramp"))) {
-			log.info("the trigger just triggered, adding menu item");
-			menuManager.get().addPlayerMenuItem(KICK_CHARLIE);
-			menuManager.addNpcMenuOption();
-
-		}
-//		if (Arrays.stream(client.getPlayerOptions()).noneMatch(KICK_CHARLIE::equals)) {
-//			menuManager.get().addPlayerMenuItem(KICK_CHARLIE);
-//		}
-	}
+//	@Subscribe
+//	public void onClientTick(ClientTick e) {}
 
 	@Subscribe
-	public void onMenuEntryAdded(MenuEntryAdded event) {
-		addMenuItem();
-	}
-
-
-	@Subscribe
-	public void onMenuOptionClicked(MenuOptionClicked e)
+	public void onChatMessage(ChatMessage event)
 	{
-		log.info(e.toString());
-		log.info(e.getMenuTarget().toString());
-		if (e.getMenuTarget().endsWith("Tramp"))
+		if (!isKickQueued) { return; } // Removing this allows any chat message to trigger the kick, which is not desired.
+		if (event.getType() == ChatMessageType.ENGINE && event.getMessage().contains("Nothing interesting happens"))
 		{
-			log.info("charlie location assigned");
-			charlieLocation = LocalPoint.fromScene(e.getParam0(), e.getParam1());
-		}
-		else
-		{
-			charlieLocation = null;
+			log.info("Charlie the Tramp kicked successfully.");
+			executeKick();
 		}
 	}
 
 	@Subscribe
-	public void onClientTick(ClientTick e)
-	{
-		if (charlieLocation != null)
+	public void onMenuOptionClicked(MenuOptionClicked event){
+		// MenuOptionClicked(getParam0=0, getParam1=0, getMenuOption=Use, getMenuTarget=<col=ff9040>Leather boots</col><col=ffffff> -> <col=ffff00>Charlie the Tramp, getMenuAction=WIDGET_TARGET_ON_NPC, getId=26381)
+		if (event.getMenuOption().equals("Use") && event.getMenuTarget().contains("boots") && event.getMenuTarget().endsWith("Tramp"))
 		{
-			Player local = client.getLocalPlayer();
-			if (charlieLocation.distanceTo(local.getLocalLocation()) <= Perspective.LOCAL_TILE_SIZE)
-			{
-				local.setAnimation(423);
-				local.setActionFrame(0);
-				charlieLocation = null;
-			}
+			log.info("Kick queued for Charlie the Tramp with boots: " + event.getMenuTarget());
+			isKickQueued = true;
 		}
+
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
-	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
-		}
+	private void executeKick() {
+		Player local = client.getLocalPlayer();
+		client.playSoundEffect(2565);
+		local.setAnimation(423);
+		local.setActionFrame(0);
+		client.addChatMessage(ChatMessageType.PUBLICCHAT, local.getName(), "DIE DIE DIE", null);
+		log.info("Kick executed.");
+		// Reset the kick queue
+		isKickQueued = false;
+	}
+
+	private void executePunch() {
+
 	}
 
 	@Provides
